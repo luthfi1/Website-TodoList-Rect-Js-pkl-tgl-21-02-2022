c0774666dcb30770d59ed43c613d12aaf34ef651
Muhammad Luthfi Farizqi <68719137+luthfi1@users.noreply.github.com>
2022-02-21 14:38:58 +0700

    Your Message

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const date = new Date();
+
+function Footer(){
+  return(
+    <footer>
+      <p> Copyright © 2022 Muhammad Luthfi Farizqi. </p>
+    </footer>
+  )
+}
+
+export default Footer;
